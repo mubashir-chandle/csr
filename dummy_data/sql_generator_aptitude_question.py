5b38052d7ab0408b36dd2d23d0f70a6ceca70f08
mubashir-chandle <61664272+mubashir-chandle@users.noreply.github.com>
2020-03-09 22:51:27 +0530

    Changed the way in which aptitude test questions with images are generated.

--- dummy_data/sql_generator_aptitude_question.py
+++ dummy_data/sql_generator_aptitude_question.py
@@ -13,7 +13,7 @@ with open('generated_sql_aptitude_questions.txt', 'w') as f:
     for category in categories:
         for i in range(10):
             correct_option = random.randint(1, 4)
-            if counter % 7 == 0:
+            if random.randint(1, 5) == 1:
                 f.write(f'INSERT INTO aptitude_question VALUES({counter}, "{category}", "Question {counter} text", "Option 1", "Option 2", "Option 3", "Option 4", {correct_option}, "placeholder");\n')
             else:
                 f.write(f'INSERT INTO aptitude_question("id",  "category", "question", "option_1", "option_2", "option_3", "option_4", "correct_option") VALUES({counter}, "{category}", "Question {counter} text", "Option 1", "Option 2", "Option 3", "Option 4", {correct_option});\n')
